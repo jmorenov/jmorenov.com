624a86c37e9028666ab05a772ead96aef0c3c399
Javier Moreno <javmorenov@gmail.com>
2018-02-03 00:05:31 +0100

    Added redirect to https

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import MainPage from './pages/Main/MainPage';
-//import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker';
 import unregister from './registerServiceWorker';
 
-ReactDOM.render(<MainPage />, document.getElementById('root'));
-//registerServiceWorker();
-unregister();
+if (window.location.protocol !== 'https:') {
+    window.location = 'https:' + window.location.href.substring(window.location.protocol.length);
+} else {
+    ReactDOM.render(<MainPage />, document.getElementById('root'));
+    registerServiceWorker();
+    //unregister();
+}

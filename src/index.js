9a3e7357a521d035831dbf2da5e35c0a6002600b
Javier Moreno Vega <jmorenov28@gmail.com>
2018-02-04 23:14:27 +0100

    Removed a warning

--- src/index.js
+++ src/index.js
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import MainPage from './pages/Main/MainPage';
 import registerServiceWorker from './registerServiceWorker';
-import unregister from './registerServiceWorker';
 
 if (window.location.protocol !== 'https:') {
     window.location = 'https:' + window.location.href.substring(window.location.protocol.length);
 } else {
     ReactDOM.render(<MainPage />, document.getElementById('root'));
     registerServiceWorker();
-    //unregister();
 }
